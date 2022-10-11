713d67fc539f5f1db324d8158795b1d4bc7c189c
fmshot2 <femishotolaa@gmail.com>
2022-10-11 11:12:11 +0100

    removed comments from header.js

--- header.js
+++ header.js
@@ -7,29 +7,7 @@ hamburger.addEventListener('click', () => {
   const imgdiv = document.querySelector('.imgdiv');
   const headerBrand = document.querySelector('.headerBrand');
   hamburger.classList.toggle('active');
-  // navigate.classList.toggle('active');
   headerItems.classList.toggle('active');
   imgdiv.classList.toggle('greyed_out');
   headerBrand.classList.toggle('greyed_out');
 });
-// const toggle = document.querySelector('#toggle');
-// toggle.addEventListener('click', () => {
-//   const navigate = document.querySelector('.navigate');
-//   const hamburger = document.querySelector('.hamburger');
-//   hamburger.classList.toggle('active');
-//   navigate.classList.toggle('active');
-// });
-// const toggle2 = document.querySelector('#toggle2');
-// toggle2.addEventListener('click', () => {
-//   const navigate = document.querySelector('.navigate');
-//   const hamburger = document.querySelector('.hamburger');
-//   hamburger.classList.toggle('active');
-//   navigate.classList.toggle('active');
-// });
-// const toggle3 = document.querySelector('#toggle3');
-// toggle3.addEventListener('click', () => {
-//   const navigate = document.querySelector('.navigate');
-//   const hamburger = document.querySelector('.hamburger');
-//   hamburger.classList.toggle('active');
-//   navigate.classList.toggle('active');
-// });
