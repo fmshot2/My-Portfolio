1860e67e9ec9baa04bfc44a40b7aa54e2e8546f3
fmshot2 <femishotolaa@gmail.com>
2022-10-12 07:22:28 +0100

    removed active on click of portfolio link

--- navbar.js
+++ navbar.js
@@ -1,8 +1,19 @@
 const hamburger = document.querySelector('#clicktoggle');
 hamburger.addEventListener('click', () => {
-  console.log('ggg', 'fgfgf');
   const hamburger = document.querySelector('.hamburger');
   const navigate = document.querySelector('.navigate');
+  const headerItems = document.querySelector('.header-items');
+  const imgdiv = document.querySelector('.imgdiv');
+  const headerBrand = document.querySelector('.headerBrand');
+  hamburger.classList.toggle('active');
+  headerItems.classList.toggle('active');
+  imgdiv.classList.toggle('greyed_out');
+  headerBrand.classList.toggle('greyed_out');
+});
+
+const projects = document.querySelector('#portfolio');
+projects.addEventListener('click', () => {
+  const hamburger = document.querySelector('.hamburger');
   const headerItems = document.querySelector('.header-items');
   const imgdiv = document.querySelector('.imgdiv');
   const headerBrand = document.querySelector('.headerBrand');
