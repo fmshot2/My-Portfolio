a30d45027ed9ee6ce99c9f21636a29f0aee01ee3
fmshot2 <femishotolaa@gmail.com>
2022-10-14 10:51:09 +0100

    local storage done

--- localstorage.js
+++ localstorage.js
@@ -1,37 +1,23 @@
-var storedValue = [];
-const form = document.getElementById('form');
-const save_in_local_storage = () => {
-  // form.addEventListener('change', () => {
-    const thedata = {
-      // name: document.getElementById('form_name').value,
-      name: document.querySelector('.input-for-name').value,
-      email: document.getElementById('id-for-email').value,
-      comment: document.querySelector('.input-for-textarea').value,
 
-      // email: document.getElementById('form_email').value,
-      // comment: document.getElementById('form_message').value,
-    };
-    storedValue.push(thedata);
-    localStorage.setItem('formdata', JSON.stringify(storedValue));
-  // });
+const storedValue = [];
+const contact_form = document.getElementById('id-of-form');
+
+contact_form.addEventListener('change', () => {
+  const thedata = {
+    name: document.querySelector('.input-for-name').value,
+    email: document.getElementById('email-field').value,
+    comment: document.querySelector('.input-for-textarea').value,
+  };
+  storedValue[0] = thedata;
+  localStorage.setItem('formdata', JSON.stringify(storedValue));
+});
+
+function fetch_stored_data() {
+  const fetched_data = JSON.parse(localStorage.getItem('formdata'));
+  document.querySelector('.input-for-name').value  = fetched_data[0].name;
+  document.getElementById('email-field').value = fetched_data[0].email;
+  document.querySelector('.input-for-textarea').value = fetched_data[0].comment;
+
 }
-
-const fetch_stored_data = () => {
-  const receive = JSON.parse(localStorage.getItem('formdata'));
-  
-  document.querySelector('.input-for-name').value  = receive[0].name;
-  
-  document.querySelector('id-for-email').value = receive[0].email;
-   document.querySelector('.input-for-textarea').value = receive[0].comment;
-
-  // document.getElementById('form_email').value = receive[0].email;
-  // document.getElementById('form_name').value = receive[0].name;
-  // document.getElementById('form_message').value = receive[0].comment;
-}
-form.addEventListener('submit', save_in_local_storage());
+// local_form.addEventListener('submit', save_in_local_storage());
 window.addEventListener('DOMContentLoaded', fetch_stored_data());
-
-
-module.exports = {
-  save_in_local_storage, fetch_stored_data,
-};
