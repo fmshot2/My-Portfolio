339db0efe6e9fc27aee5ab1bf496117c117220b2
fmshot2 <femishotolaa@gmail.com>
2022-10-14 05:56:24 +0100

    started local storage feature

--- localstorage.js
+++ localstorage.js
@@ -1,22 +1,22 @@
-const storagedata = [];
+const storedValue = [];
 const form = document.getElementById('form');
-function submitt() {
+function save_in_local_storage() {
   form.addEventListener('change', () => {
     const thedata = {
       name: document.getElementById('form_name').value,
       email: document.getElementById('form_email').value,
       comment: document.getElementById('form_message').value,
     };
-    storagedata.unshift(thedata);
-    localStorage.setItem('formdata', JSON.stringify(storagedata));
+    storedValue.unshift(thedata);
+    localStorage.setItem('formdata', JSON.stringify(storedValue));
   });
 }
 
-function callData() {
+function fetch_stored_data() {
   const receive = JSON.parse(localStorage.getItem('formdata'));
   document.getElementById('form_email').value = receive[0].email;
   document.getElementById('form_name').value = receive[0].name;
   document.getElementById('form_message').value = receive[0].comment;
 }
-form.addEventListener('submit', submitt());
-window.addEventListener('DOMContentLoaded', callData());
+form.addEventListener('submit', save_in_local_storage());
+window.addEventListener('DOMContentLoaded', fetch_stored_data());
