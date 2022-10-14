93e50879ff8665d41e0761aef54d0ebdf65bc8e3
fmshot2 <femishotolaa@gmail.com>
2022-10-14 11:12:04 +0100

    still working on local storage

--- localstorage.js
+++ localstorage.js
@@ -12,12 +12,6 @@ contact_form.addEventListener('change', () => {
   localStorage.setItem('formdata', JSON.stringify(storedValue));
 });
 
-function fetch_stored_data() {
-  const fetched_data = JSON.parse(localStorage.getItem('formdata'));
-  document.querySelector('.input-for-name').value  = fetched_data[0].name;
-  document.getElementById('email-field').value = fetched_data[0].email;
-  document.querySelector('.input-for-textarea').value = fetched_data[0].comment;
 
-}
+
 // local_form.addEventListener('submit', save_in_local_storage());
-window.addEventListener('DOMContentLoaded', fetch_stored_data());
